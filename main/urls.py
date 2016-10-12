5801a48d2f4dd591563da2726d000330a6b0e900
serrrgggeee <crackkc@mail.ru>
2016-10-12 14:27:53 +0300

    [file] delite file

--- main/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url, include
-
-from .import views
-
-
-urlpatterns = [
-    url(r'^$', views.ListOktMainView.as_view()),
-
-]
--- place/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-
-from .import views
-
-
-urlpatterns = [
-    url(r'/(?P<id>\d+)/$', views.SinglePlaceView.as_view(), name='single_place'),
-
-]
