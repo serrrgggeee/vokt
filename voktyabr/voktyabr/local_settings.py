269913ea22c34a023912ad29723f02779524167a
serrrgggeee <crackkc@mail.ru>
2016-10-13 08:05:40 +0300

    [files] remove local_settings

--- voktyabr/voktyabr/local_settings.py
+++
@@ -1,8 +0,0 @@
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'vokt',
-        'USER': 'postgres',
-        'PASSWORD':'Tktyf,firjdf1'
-    }
-}
